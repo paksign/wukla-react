4ab89a008d55f4dd19afc6f916de048d40fbc152
Ali Zahid <ali.zahid@live.com>
2024-05-02 15:19:23 +0500

    Dependencies upgraded

--- src/app.tsx
+++ src/app.tsx
@@ -7,19 +7,26 @@ import { document } from "./document";
 export function App() {
   return (
     <DocumentEditor.Root document={document}>
-      <div className="flex gap-4 p-4">
-        <DocumentEditor.Canvas className="fixed inset-0 h-screen w-screen" />
+      <DocumentEditor.Canvas
+        position="fixed"
+        inset="0"
+        height="100vh"
+        width="100vw"
+      />
 
-        <DocumentEditor.SideBar
-          onSubmit={(data) => {
-            console.log("onSubmit", data);
-          }}
-          onSend={(id) => {
-            console.log("onSend", id);
-          }}
-          className="fixed shadow-2xl right-4 top-4 max-h-[600px] bg-white rounded-lg border border-gray-200"
-        />
-      </div>
+      <DocumentEditor.SideBar
+        onSave={(data) => {
+          console.log("onSave", data);
+        }}
+        onSend={(id) => {
+          console.log("onSend", id);
+        }}
+        top="4"
+        right="4"
+        maxHeight="600px"
+        position="fixed"
+        className="shadow-2xl bg-white rounded-lg border border-gray-200"
+      />
     </DocumentEditor.Root>
   );
 }
